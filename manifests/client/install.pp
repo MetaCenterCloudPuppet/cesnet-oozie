68e298d0224417c0c65b1eee5fc0568f20cb6339
František Dvořák <valtri@civ.zcu.cz>
2015-01-22 22:49:05 +0100

    Set alternatives also for client.

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -2,6 +2,9 @@
 #
 class oozie::client::install {
   include stdlib
+  contain oozie::common::postinstall
 
   ensure_packages($oozie::packages['client'])
+
+  Package[$oozie::packages['client']] -> Class['oozie::common::postinstall']
 }
