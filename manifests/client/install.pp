9ef8a542663c2214a1ecf3c54fea8c3f453d84b3
František Dvořák <valtri@civ.zcu.cz>
2015-04-16 13:41:02 +0200

    Not using alternatives for the client (/etc/oozie/conf only in the server). Issue #1.

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -2,9 +2,6 @@
 #
 class oozie::client::install {
   include stdlib
-  contain oozie::common::postinstall
 
   ensure_packages($oozie::packages['client'])
-
-  Package[$oozie::packages['client']] -> Class['oozie::common::postinstall']
 }
