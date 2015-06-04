606224d955c39ec05c1f93b599be471c6f79c7c0
František Dvořák <valtri@civ.zcu.cz>
2015-06-04 16:54:42 +0200

    Use augeas for environment files (it will be more generic on version or distribution changes).

--- manifests/common/config.pp
+++ manifests/common/config.pp
@@ -11,9 +11,20 @@ class oozie::common::config {
   }
 
   file { "${::oozie::confdir}/oozie-env.sh":
-    owner   => 'oozie',
-    group   => 'oozie',
-    mode    => '0600',
-    content => template('oozie/oozie-env.sh.erb'),
+    owner => 'oozie',
+    group => 'oozie',
+    mode  => '0600',
+  }
+
+  $env = "${::oozie::confdir}/oozie-env.sh"
+  augeas { "${::oozie::confdir}/oozie-env.sh-https":
+    incl    => $env,
+    lens    => 'Shellvars.lns',
+    changes => [
+      "set /files${env}/OOZIE_HTTPS_PORT 11443",
+      "set /files${env}/OOZIE_HTTPS_PORT/export ''",
+      "set /files${env}/OOZIE_HTTPS_KEYSTORE_PASS '${::oozie::https_keystore_password}'",
+      "set /files${env}/OOZIE_HTTPS_KEYSTORE_PASS/export ''",
+    ],
   }
 }
