bd2e4bc49d87c8d4750686cf787059c07cdd2afa
František Dvořák <valtri@civ.zcu.cz>
2016-04-02 20:02:54 +0200

    Set proper Oozie base URL.
    Maybe it is not needed, but better to hae it right as it is in all logs.

--- manifests/common/config.pp
+++ manifests/common/config.pp
@@ -16,13 +16,25 @@ class oozie::common::config {
     mode  => '0600',
   }
 
+  if $::oozie::https {
+    $base_url = 'https://${OOZIE_HTTP_HOSTNAME}:${OOZIE_HTTPS_PORT}/oozie'
+  } else {
+    $base_url = 'http://${OOZIE_HTTP_HOSTNAME}:${OOZIE_HTTP_PORT}/oozie'
+  }
+
   $env = "${::oozie::confdir}/oozie-env.sh"
   augeas { "${::oozie::confdir}/oozie-env.sh-https":
     incl    => $env,
     lens    => 'Shellvars.lns',
     changes => [
+      "set /files${env}/OOZIE_HTTP_HOSTNAME '`hostname -f`'",
+      "set /files${env}/OOZIE_HTTP_HOSTNAME/export ''",
+      "set /files${env}/OOZIE_HTTP_PORT 11000",
+      "set /files${env}/OOZIE_HTTP_PORT/export ''",
       "set /files${env}/OOZIE_HTTPS_PORT 11443",
       "set /files${env}/OOZIE_HTTPS_PORT/export ''",
+      "set /files${env}/OOZIE_BASE_URL '${base_url}'",
+      "set /files${env}/OOZIE_BASE_URL/export ''",
       "set /files${env}/OOZIE_HTTPS_KEYSTORE_PASS '${::oozie::https_keystore_password}'",
       "set /files${env}/OOZIE_HTTPS_KEYSTORE_PASS/export ''",
     ],
