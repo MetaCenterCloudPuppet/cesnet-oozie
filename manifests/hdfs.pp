38709ba51fbe294c6e2c7eabf48987edc95b4bdc
František Dvořák <valtri@civ.zcu.cz>
2017-04-19 00:31:05 +0200

    Comment typos

--- manifests/hdfs.pp
+++ manifests/hdfs.pp
@@ -1,6 +1,6 @@
 # == Class ::oozie::hdfs
 #
-# HDFS initialiations. Actions necessary to launch on HDFS namenode: Create oozie user, if needed. Creates directory /user/oozie on HDFS for Oozie. It needs to be called after Hadoop HDFS is working (its namenode and proper number of datanodes) and before Oozie service startup.
+# HDFS initializations. Actions necessary to launch on HDFS namenode: Creates oozie user, if needed. Creates directory /user/oozie on HDFS for Oozie. It needs to be called after Hadoop HDFS is working (its namenode and proper number of datanodes) and before Oozie service startup.
 #
 # This class is needed to be launched on one HDFS namenode. With some limitations it can be launched on any Hadoop node (user oozie created or oozie installed on namenode, kerberos ticket available on the local node).
 #
